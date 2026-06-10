2973f3691223560f82990f008c80fd0a9642ac90
luyan-tamec <contatestxxx6@gmail.com>
2026-06-09 22:11:42 -0300

    fix

--- server.js
+++ server.js
@@ -18,6 +18,8 @@ const PORT = process.env.PORT || 3001;
 
 // ── CORS ──────────────────────────────────────────────────────────────────────
 const allowedOrigins = [
+    "https://roleta-admin.onrender.com",
+    "https://luyan-tamec.github.io/roleta-leoeisa",
   "http://localhost:3000",
   "http://localhost:3001",
   "http://localhost:5500",
